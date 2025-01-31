be9ce00591eabb0cbca43fe3203a22e510972985
ispeedonthe405 <ispeedonthe405@gmail.com>
2025-01-30 20:50:28 -0800

    Add project files.

---
+++ Test/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Test
+{
+    internal class Program
+    {
+
+
+
+		///////////////////////////////////////////////////////////
+		#region INotifyPropertyChanged
+
+		public event PropertyChangedEventHandler? PropertyChanged;
+
+		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		protected void SetField<TField>(ref TField field, TField value, string propertyName)
+		{
+			if (EqualityComparer<TField>.Default.Equals(field, value))
+			{
+				return;
+			}
+
+			field = value;
+			OnPropertyChanged(propertyName);
+		}
+
+		#endregion INotifyPropertyChanged
+		///////////////////////////////////////////////////////////
+
+
+
+
+
+		/////////////////////////////////////////////////////////
+		#region something
+
+		#endregion something
+		/////////////////////////////////////////////////////////
+
+		int _thing;
+
+
+        public int thing
+        {
+            get => _thing;
+            set => SetField(ref _thing, value, nameof(thing));
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
